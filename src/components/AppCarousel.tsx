fc15831d0fa059b0c2973abd81488f8a4f40630e
Thiago Santos <thiagosantosmunhoz@gmail.com>
2024-07-03 18:37:24 -0300

    Alterações e correção no componente AppCarousel

--- src/components/AppCarousel.tsx
+++ src/components/AppCarousel.tsx
@@ -1,16 +1,16 @@
 "use client";
 
 import { Carousel } from "flowbite-react";
+import Image from "next/image";
 
 export const AppCarousel = () => {
   return (
-    <div className="h-56 sm:h-[800px] ">
+    <div className="h-56 sm:h-[800px]">
       <Carousel>
-        <img src="foto4.jpg" alt="..." />
-        <img src="foto1.jpg" alt="..." />
-        <img src="foto2.jpg" alt="..." />
-        <img src="foto3.jpg" alt="..." />
-        {/* <img src="fequer_16.jpg" alt="..." /> */}
+        <Image src="/foto4.jpg" width={800} height={600} alt="Foto 4" />
+        <Image src="/foto1.jpg" width={800} height={600} alt="Foto 1" />
+        <Image src="/foto2.jpg" width={800} height={600} alt="Foto 2" />
+        <Image src="/foto3.jpg" width={800} height={600} alt="Foto 3" />
       </Carousel>
     </div>
   );
